0fb617cb2dc1728546926e3a41787e0bf17cf953
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-11-08 12:18:38 +0200

    stop animation in card

--- lib/screen/card_data.dart
+++ lib/screen/card_data.dart
@@ -30,35 +30,21 @@ class AnimatedCard extends StatefulWidget {
   _AnimatedCardState createState() => _AnimatedCardState();
 }
 
-class _AnimatedCardState extends State<AnimatedCard> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
+class _AnimatedCardState extends State<AnimatedCard> {
   final List<Map<String, String>> categories = [
     {'title': 'المستشفيات', 'image': 'hospital.jpg', 'item': 'مستشفى'},
     {'title': 'مراكز الأشعة', 'image': 'scan.jpg', 'item': 'مراكز الأشعة'},
-    {'title': 'معامل التحاليل', 'image': 'medicaltests.jpg', 'item': 'معمل تحاليل'},
+    {
+      'title': 'معامل التحاليل',
+      'image': 'medicaltests.jpg',
+      'item': 'معمل تحاليل'
+    },
     {'title': 'مراكز متخصصة', 'image': 'clinic.jpg', 'item': 'مركز متخصص'},
     {'title': 'العيادات', 'image': 'clinic.jpg', 'item': 'عيادة'},
     {'title': 'الصيدليات', 'image': 'pharmacy.jpg', 'item': 'صيدلية'},
     {'title': 'العلاج الطبيعي', 'image': 'physical.jpg', 'item': 'علاج طبيعي'},
     {'title': 'البصريات', 'image': 'optometry.jpg', 'item': 'بصريات'},
-
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 2),
-      vsync: this,
-    )..repeat(reverse: true);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,56 +62,49 @@ class _AnimatedCardState extends State<AnimatedCard> with SingleTickerProviderSt
       itemBuilder: (context, index) {
         final category = categories[index];
 
-        return ScaleTransition(
-          scale: Tween<double>(begin: 0.9, end: 1.0).animate(
-            CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-          ),
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ShowData(item: category['item']!),
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ShowData(item: category['item']!),
+              ),
+            );
+          },
+          borderRadius: BorderRadius.circular(20),
+          child: Card(
+            elevation: 6,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                  child: FadeInImage(
+                    placeholder: const AssetImage('assets/images/logo.jpg'),
+                    image: AssetImage('assets/images/${category['image']}'),
+                    height: 120,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              );
-            },
-            borderRadius: BorderRadius.circular(20),
-            splashColor: Colors.red.withOpacity(0.3),
-            child: Card(
-              elevation: 12,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.circular(20),
-                    ),
-                    child: FadeInImage(
-                      placeholder: const AssetImage('assets/images/logo.jpg'),
-                      image: AssetImage('assets/images/${category['image']}'),
-                      height: 120,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 10.0), // تقليل المسافة العلوية والسفلية
+                  child: Text(
+                    category['title']!,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0), // تقليل المسافة العلوية والسفلية
-                    child: Text(
-                      category['title']!,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary,
-                      ),
-                    ),
-                  ),
-
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
