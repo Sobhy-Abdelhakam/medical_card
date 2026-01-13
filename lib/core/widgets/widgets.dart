a1330dc8e2f02866c8366c85415ecb9f8fee7808
Sobhy <sobhy.abdelhakam25@gmail.com>
2026-01-13 15:28:36 +0200

    delete unused files

--- lib/core/widgets/widgets.dart
+++
@@ -1,8 +0,0 @@
-/// Core widgets barrel file
-library;
-
-export 'app_button.dart';
-export 'app_card.dart';
-export 'app_loading.dart';
-export 'app_state_widgets.dart';
-export 'app_text_field.dart';
